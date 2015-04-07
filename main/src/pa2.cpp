e3437ec6325b18c12c9d535973dcc693de47476c
leafriend <leafriend@gmail.com>
2015-04-08 02:37:20 +0900

    기본 코드 추가

---
+++ main/src/pa2.cpp
@@ -0,0 +1,4 @@
+#include "pa2.h"
+
+void pa2(Macro** macros, Pico** picos, Mobile** mobiles) {
+}
---
+++ main/src/pa2.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "macro.h"
+#include "pico.h"
+#include "mobile.h"
+
+void pa2(Macro** macros, Pico** picos, Mobile** mobiles);

e862e747780685920ea3cb680115cd21a1ec4964
leafriend <leafriend@gmail.com>
2015-04-09 12:13:26 +0900

    매크로가 꺼졌을 때 피코 연결 값 계산 추가

--- main/src/pa2.cpp
+++ main/src/pa2.cpp
@@ -31,7 +31,7 @@ void pa2(Macro** macros, Pico** picos, Mobile** mobiles) {
 		FOREACH_MACROS {
 
 			if (!macros[mac]->get_state())
-				continue;
+				;//continue;
 
 			double macro_best_sum_lambda_r = DBL_MIN;
 			pa2_find_best_mobile_state(macros[mac], &macro_best_sum_lambda_r);
@@ -49,17 +49,19 @@ void pa2(Macro** macros, Pico** picos, Mobile** mobiles) {
 
 			FOREACH_MACROS
 				best_macro_states[mac] = macros[mac]->get_state();
-
+			
 		}
 
 	}
 
 	FOREACH_MACROS
 		macros[mac]->set_state(best_macro_states[mac]);
+	
+	printf("SATAE   "); for (int mac = NUM_MACRO; mac --> 0;) printf("%d", macros[mac]->get_state()); printf("\n");
 
 }
 
-void pa2_find_best_mobile_state(Macro* macro, double* best_sum_lambda_r) {
+void pa2_find_best_mobile_state(Macro* macro, double* macro_best_sum_lambda_r) {
 
 	// TODO 매크로의 모바일 갯수가 사이즈를 넘어가는 경우 배열 등으로 변경
 	int NUM_MOBILE_TS = macro->get_num_mobiles_to_service();
@@ -70,63 +72,95 @@ void pa2_find_best_mobile_state(Macro* macro, double* best_sum_lambda_r) {
 
 	Macro_Mobile** mmobiles = macro->get_mobiles_to_service();
 
-	bool* best_mobile_conn_macros = (bool*) malloc(sizeof(bool) * NUM_MOBILE_TS);
+	if (macro->get_state()) {
+		// 매크로가 켜져 있는 경우
+		// : 각 모바일이 매크로와 피코 연결 상태의 r값 중 최대 값을 찾음
 
-	for (int S = 0; S < num_mobile_state; S++) {
+		bool* best_mobile_conn_macros = (bool*) malloc(sizeof(bool) * NUM_MOBILE_TS);
 
-		double curr_sum_lambda_r = 0.0;
+		for (int S = 0; S < num_mobile_state; S++) {
 
-		// Mobile 연결 상태 지정
-		FOREACH_MOBILES_TS {
-			Mobile* mobile = (Mobile*) mmobiles[mob]->get_mobile();
-			mobile->conn_macro = 1 == ((1 << mob) & S) >> mob;
-		}
+			double curr_sum_lambda_r = 0.0;
 
-		FOREACH_RBS {
-
+			// Mobile 연결 상태 지정
 			FOREACH_MOBILES_TS {
 				Mobile* mobile = (Mobile*) mmobiles[mob]->get_mobile();
+				mobile->conn_macro = 1 == ((1 << mob) & S) >> mob;
+			}
 
-				if (mobile->conn_macro) {
-					// Macro에 연결한 경우
-					curr_sum_lambda_r += mobile->lambda * mobile->get_macro_throughput(ri);
+			FOREACH_RBS {
 
-				} else {
-					// Pico에 연결한 경우
+				FOREACH_MOBILES_TS {
+					Mobile* mobile = (Mobile*) mmobiles[mob]->get_mobile();
 
-					if (mobile->get_pico()->get_pico()->is_abs()) {
-						// ABS인 경우
-						curr_sum_lambda_r += mobile->lambda * mobile->get_abs_pico_throughput(ri);
+					if (mobile->conn_macro) {
+						// Macro에 연결한 경우
+						curr_sum_lambda_r += mobile->lambda * mobile->get_macro_throughput(ri);
 
 					} else {
-						// non-ABS인 경우
-						curr_sum_lambda_r += mobile->lambda * mobile->get_pico_throughput(ri);
+						// Pico에 연결한 경우
+
+						if (mobile->get_pico()->get_pico()->is_abs()) {
+							// ABS인 경우
+							curr_sum_lambda_r += mobile->lambda * mobile->get_abs_pico_throughput(ri);
+
+						} else {
+							// non-ABS인 경우
+							curr_sum_lambda_r += mobile->lambda * mobile->get_pico_throughput(ri);
+						}
+
 					}
 
 				}
 
 			}
 
+			if (curr_sum_lambda_r > *macro_best_sum_lambda_r) {
+
+				*macro_best_sum_lambda_r = curr_sum_lambda_r;
+
+				FOREACH_MOBILES_TS
+					best_mobile_conn_macros[mob] = mmobiles[mob]->get_mobile()->conn_macro;
+
+			}
+
 		}
 
-		if (curr_sum_lambda_r > *best_sum_lambda_r) {
+		FOREACH_RBS {
+			FOREACH_MOBILES_TS {
+				Mobile* mobile = (Mobile*) mmobiles[mob]->get_mobile();
+				mobile->conn_macro = best_mobile_conn_macros[mob];
+			}
+		}
+		
+		free(best_mobile_conn_macros);
 
-			*best_sum_lambda_r = curr_sum_lambda_r;
+	} else {
+		// 매크로가 꺼져 있는 경우
 
-			FOREACH_MOBILES_TS
-				best_mobile_conn_macros[mob] = mmobiles[mob]->get_mobile()->conn_macro;
+		double curr_sum_lambda_r = 0.0;
+		
+		FOREACH_RBS {
+
+			FOREACH_MOBILES_TS {
+				Mobile* mobile = (Mobile*) mmobiles[mob]->get_mobile();
+				// Pico에 연결한 경우
+
+				if (mobile->get_pico()->get_pico()->is_abs()) {
+					// ABS인 경우
+					curr_sum_lambda_r += mobile->lambda * mobile->get_abs_pico_throughput(ri);
+
+				} else {
+					// non-ABS인 경우
+					curr_sum_lambda_r += mobile->lambda * mobile->get_pico_throughput(ri);
+				}
+
+			}
 
 		}
+
+		*macro_best_sum_lambda_r = curr_sum_lambda_r;
 
 	}
-
-	FOREACH_RBS {
-		FOREACH_MOBILES_TS {
-			Mobile* mobile = (Mobile*) mmobiles[mob]->get_mobile();
-			mobile->conn_macro = best_mobile_conn_macros[mob];
-		}
-	}
-
-	free(best_mobile_conn_macros);
 
 }

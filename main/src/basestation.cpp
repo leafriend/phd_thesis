be9f913be91892cf258cbe049082472776b49a2b
leafriend <leafriend@gmail.com>
2015-04-14 09:12:08 +0900

    BaseStation 정의 작성

---
+++ main/src/basestation.cpp
@@ -0,0 +1,17 @@
+#include "basestation.h"
+
+BaseStation::BaseStation(int idx, double x, double y, double tx_power)
+: idx(idx), x(x), y(y), tx_power(tx_power)
+{
+	num_mobiles_to_service = 0;
+
+	pa3_lambda_r = 1.0;
+}
+
+int BaseStation::get_num_mobiles_to_service() const {
+	return num_mobiles_to_service;
+}
+
+Macro_Mobile** BaseStation::get_mobiles_to_service() {
+	return mobiles_to_service;
+}

014ac291eb35f7a3925e86b58e12dce7903ebc11
leafriend <leafriend@gmail.com>
2015-04-14 09:26:21 +0900

    Edge 정의 작성

---
+++ main/src/edge.cpp
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+#include "func.h"
+#include "basestation.h"
+#include "mobile.h"
+#include "edge.h"
+
+Edge::Edge(BaseStation* bs, Mobile* mobile)
+	: bs(bs), mobile(mobile), distance(sqrt(pow((bs->x - mobile->x), 2) + pow((bs->y - mobile->y), 2)))
+, channel_gain_factor(bs->tx_power * pow((1 / distance), (double) PATH_LOSS_EXPO))
+{
+}
+
+void Edge::generate_channel_gain() {
+	FOREACH_RBS
+		channel_gain[ri]
+			= channel_gain_factor
+			* RAYLEIGH()
+			* LOG_NORMAL()
+		;
+}
+
+double Edge::get_channel_gain(int ri) {
+	return channel_gain[ri];
+}

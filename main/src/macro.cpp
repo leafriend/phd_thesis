b29fd6dbd2798f9075a6ea74042f8f84386cfd16
leafriend <leafriend@gmail.com>
2015-04-14 09:14:56 +0900

    Macro가 BaseStation을 상속받도록 수정

--- main/src/macro.cpp
+++ main/src/macro.cpp
@@ -13,23 +13,12 @@
 #include "macro.h"
 
 Macro::Macro(int idx, double x, double y, double tx_power)
-: idx(idx), x(x)
-, y(y)
-, tx_power(tx_power)
+	: BaseStation(idx, x, y, tx_power)
 {
-	num_mobiles_to_service = 0;
 	num_mobiles_interfered = 0;
 	allocation_count = 0;
 
 	pa3_lambda_r = 1.0;
-}
-
-int Macro::get_num_mobiles_to_service() const {
-	return num_mobiles_to_service;
-}
-
-Macro_Mobile** Macro::get_mobiles_to_service() {
-	return mobiles_to_service;
 }
 
 void Macro::count_allocation() {
--- main/src/macro.h
+++ main/src/macro.h
@@ -4,6 +4,7 @@
 
 #include "mobile.h"
 #include "macro_mobile.h"
+#include "basestation.h"
 //class Mobile;
 //class Macro_Mobile;
 
@@ -12,20 +13,11 @@ enum MacroState {
 	ON,
 };
 
-class Macro {
+class Macro : public BaseStation {
 
 	friend class Macro_Mobile;
 
-public:
-	const int idx;
-	const double x;
-	const double y;
-	const double tx_power;
-
 private:
-
-	Macro_Mobile* mobiles_to_service[NUM_MM];
-	int num_mobiles_to_service;
 
 	Macro_Mobile* mobiles_interfered[NUM_PM];
 	int num_mobiles_interfered;
@@ -47,9 +39,6 @@ public:
 public:
 
 	Macro(int idx, double x, double y, double tx_power);
-
-	int get_num_mobiles_to_service() const;
-	Macro_Mobile** get_mobiles_to_service();
 
 	void count_allocation();
 
